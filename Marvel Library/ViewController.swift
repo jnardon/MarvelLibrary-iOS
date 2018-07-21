d92a97b3be9afc969fa0a8c5f6e6910d5b47e562
Juliano <jnardon@gmail.com>
2018-07-21 12:47:13 -0300

    first commit

---
+++ Marvel Library/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Marvel Library
+//
+//  Created by Juliano Krieger Nardon on 21/07/18.
+//  Copyright © 2018 CWI Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
